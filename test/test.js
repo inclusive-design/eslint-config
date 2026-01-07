4c7528d69ba679df30d3efceb9862990f0b36cac
Ned Zimmerman <nzimmerman@ocadu.ca>
2026-01-07 15:47:32 -0400

    fix: resolve linting errors

--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 import config from '../index.js';
-import test from "node:test";
-import assert from "node:assert";
+import test from 'node:test';
+import assert from 'node:assert';
 
 test('eslintConfig', async () => {
-	assert.ok(Array.isArray(config))
+	assert.ok(Array.isArray(config));
 });
