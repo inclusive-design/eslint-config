cba67f293106e7c48f7344e4ad68723c052208cd
Ned Zimmerman <nzimmerman@ocadu.ca>
2026-01-09 21:57:54 -0400

    feat: add linting for JSON and JSONC (resolves #17) (#22)

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import xoBrowser from 'eslint-config-xo/browser';
 import eslintPluginUnicorn from 'eslint-plugin-unicorn';
 import jsdoc from 'eslint-plugin-jsdoc';
 import markdown from '@eslint/markdown';
+import json from '@eslint/json';
 
 export default defineConfig([
 	{
@@ -12,6 +13,19 @@ export default defineConfig([
 			eslintPluginUnicorn.configs.recommended,
 			jsdoc.configs['flat/recommended'],
 		],
+	},
+	{
+		files: ['**/*.json'],
+		ignores: ['package-lock.json'],
+		plugins: {json},
+		language: 'json/json',
+		extends: ['json/recommended'],
+	},
+	{
+		files: ['**/*.jsonc'],
+		plugins: {json},
+		language: 'json/jsonc',
+		extends: ['json/recommended'],
 	},
 	{
 		files: ['**/*.md'],
--- test/main.test.js
+++ test/main.test.js
@@ -8,8 +8,8 @@ const hasRule = (errors, ruleId) => errors.some(error => error.ruleId === ruleId
 
 /**
  * @param {string} string The string against which ESLint should be run.
- * @param {object} config The ESLint config to use.
- * @param fileExtension
+ * @param {object} config The ESLint configuration to use.
+ * @param {string} fileExtension The extension corresponding to the file format being linted.
  * @returns {object} Messages from the lint action.
  */
 async function runEslint(string, config, fileExtension = 'js') {
@@ -26,23 +26,43 @@ test('eslintConfig', async () => {
 	assert.ok(Array.isArray(config));
 });
 
-test('jsdoc', async () => {
-	const badFixture = await readFile('./test/fixtures/jsdoc-bad.js');
+test('javascript', async () => {
+	const badFixture = await readFile('./test/fixtures/bad/javascript.js');
 	const badFixtureErrors = await runEslint(badFixture.toString(), config);
 	assert.ok(hasRule(badFixtureErrors, 'jsdoc/require-jsdoc'));
 
-	const goodFixture = await readFile('./test/fixtures/jsdoc-good.js');
+	const goodFixture = await readFile('./test/fixtures/good/javascript.js');
 	const goodFixtureErrors = await runEslint(goodFixture.toString(), config);
 	assert.ok(!hasRule(goodFixtureErrors, 'jsdoc/require-jsdoc'));
 });
 
+test('json', async () => {
+	const badFixture = await readFile('./test/fixtures/bad/json.json');
+	const badFixtureErrors = await runEslint(badFixture.toString(), config, 'json');
+	assert.ok(hasRule(badFixtureErrors, 'json/no-duplicate-keys'));
+
+	const goodFixture = await readFile('./test/fixtures/good/json.json');
+	const goodFixtureErrors = await runEslint(goodFixture.toString(), config, 'json');
+	assert.ok(!hasRule(goodFixtureErrors, 'json/no-duplicate-keys'));
+});
+
+test('jsonc', async () => {
+	const badFixture = await readFile('./test/fixtures/bad/jsonc.jsonc');
+	const badFixtureErrors = await runEslint(badFixture.toString(), config, 'jsonc');
+	assert.ok(hasRule(badFixtureErrors, 'json/no-duplicate-keys'));
+
+	const goodFixture = await readFile('./test/fixtures/good/jsonc.jsonc');
+	const goodFixtureErrors = await runEslint(goodFixture.toString(), config, 'jsonc');
+	assert.ok(!hasRule(goodFixtureErrors, 'json/no-duplicate-keys'));
+});
+
 test('markdown', async () => {
-	const badFixture = await readFile('./test/fixtures/markdown-bad.md');
+	const badFixture = await readFile('./test/fixtures/bad/markdown.md');
 	const badFixtureErrors = await runEslint(badFixture.toString(), config, 'md');
 	assert.ok(hasRule(badFixtureErrors, '@stylistic/indent'));
 	assert.ok(hasRule(badFixtureErrors, 'no-unused-vars'));
 
-	const goodFixture = await readFile('./test/fixtures/markdown-good.md');
+	const goodFixture = await readFile('./test/fixtures/good/markdown.md');
 	const goodFixtureErrors = await runEslint(goodFixture.toString(), config, 'md');
 	assert.ok(!hasRule(goodFixtureErrors, '@stylistic/indent'));
 	assert.ok(!hasRule(goodFixtureErrors, 'no-unused-vars'));
