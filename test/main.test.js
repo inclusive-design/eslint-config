e3434ffe6badfce31647a8551f408b5468f5e852
Ned Zimmerman <nzimmerman@ocadu.ca>
2026-01-09 20:05:03 -0400

    test: add tests

--- test/main.test.js
+++ test/main.test.js
@@ -1,4 +1,4 @@
-import config from '../index.js';
+import config from '../eslint.config.js';
 import test from 'node:test';
 import assert from 'node:assert';
 import {ESLint} from 'eslint';
@@ -9,15 +9,16 @@ const hasRule = (errors, ruleId) => errors.some(error => error.ruleId === ruleId
 /**
  * @param {string} string The string against which ESLint should be run.
  * @param {object} config The ESLint config to use.
+ * @param fileExtension
  * @returns {object} Messages from the lint action.
  */
-async function runEslint(string, config) {
+async function runEslint(string, config, fileExtension = 'js') {
 	const eslint = new ESLint({
 		overrideConfigFile: true,
 		overrideConfig: config,
 	});
 
-	const [firstResult] = await eslint.lintText(string);
+	const [firstResult] = await eslint.lintText(string, {filePath: `test.${fileExtension}`});
 	return firstResult.messages;
 }
 
@@ -35,15 +36,14 @@ test('jsdoc', async () => {
 	assert.ok(!hasRule(goodFixtureErrors, 'jsdoc/require-jsdoc'));
 });
 
-// Test('markdown', async () => {
-// 	const badFixture = await readFile('./test/fixtures/markdown-bad.md');
-// 	const badFixtureErrors = await runEslint(badFixture.toString(), config);
-// 	console.log(badFixtureErrors);
-// 	assert.ok(hasRule(badFixtureErrors, '@stylistic/indent'));
-// 	assert.ok(hasRule(badFixtureErrors, 'no-unused-vars'));
+test('markdown', async () => {
+	const badFixture = await readFile('./test/fixtures/markdown-bad.md');
+	const badFixtureErrors = await runEslint(badFixture.toString(), config, 'md');
+	assert.ok(hasRule(badFixtureErrors, '@stylistic/indent'));
+	assert.ok(hasRule(badFixtureErrors, 'no-unused-vars'));
 
-// 	const goodFixture = await readFile('./test/fixtures/markdown-good.md');
-// 	const goodFixtureErrors = await runEslint(goodFixture.toString(), config);
-// 	assert.ok(!hasRule(goodFixtureErrors, '@stylistic/indent'));
-// 	assert.ok(!hasRule(goodFixtureErrors, 'no-unused-vars'));
-// });
+	const goodFixture = await readFile('./test/fixtures/markdown-good.md');
+	const goodFixtureErrors = await runEslint(goodFixture.toString(), config, 'md');
+	assert.ok(!hasRule(goodFixtureErrors, '@stylistic/indent'));
+	assert.ok(!hasRule(goodFixtureErrors, 'no-unused-vars'));
+});
